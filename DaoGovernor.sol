fd089874c319cfe4efde8c78ad61335871b451b7
Jesse Tane <jesse.tane@gmail.com>
2022-03-30 00:16:54 -0400

    solc 0.8.13

--- DaoGovernor.sol
+++ DaoGovernor.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts-upgradeable/governance/GovernorUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/governance/extensions/GovernorCountingSimpleUpgradeable.sol";
